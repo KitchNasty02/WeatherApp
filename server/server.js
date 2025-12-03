71313aa3999513b9110b4f075027a9f4e65ab35d
KitchNasty02 <connorkitch10@gmail.com>
2025-12-03 10:45:00 -0600

    add cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
+// import cors from 'cors';
+
 const express = require("express");
+const cors = require("cors");
 require("./db");
 
 const PORT = 3010;
 const app = express();
+
+app.use(cors()); // allow all origins
 
 app.use(express.static("../client"));
 app.use(express.urlencoded({extended: true}));
